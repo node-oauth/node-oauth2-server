323c91b03a8ebce4593b753173ae0441d4c2b9cf
jankapunkt <jkuester@uni-bremen.de>
2023-08-22 09:02:40 +0200

    tests(unit): improve coverage for TokenModel

--- test/unit/models/token-model_test.js
+++ test/unit/models/token-model_test.js
@@ -1,4 +1,5 @@
 const TokenModel = require('../../../lib/models/token-model');
+const InvalidArgumentError = require('../../../lib/errors/invalid-argument-error');
 const should = require('chai').should();
 /**
  * Test `Server`.
@@ -6,6 +7,101 @@ const should = require('chai').should();
 
 describe('Model', function() {
   describe('constructor()', function() {
+    it('throws, if data is empty', function () {
+      try {
+        new TokenModel();
+        should.fail();
+      } catch (e) {
+        e.should.be.an.instanceOf(InvalidArgumentError);
+        e.message.should.equal('Missing parameter: `accessToken`');
+      }
+    });
+    it('throws, if `accessToken` is missing', function () {
+      const atExpiresAt = new Date();
+      atExpiresAt.setHours(new Date().getHours() + 1);
+
+      const data = {
+        client: 'bar',
+        user: 'tar',
+        accessTokenExpiresAt: atExpiresAt
+      };
+
+      try {
+        new TokenModel(data);
+        should.fail();
+      } catch (e) {
+        e.should.be.an.instanceOf(InvalidArgumentError);
+        e.message.should.equal('Missing parameter: `accessToken`');
+      }
+    });
+    it('throws, if `client` is missing', function () {
+      const atExpiresAt = new Date();
+      atExpiresAt.setHours(new Date().getHours() + 1);
+
+      const data = {
+        accessToken: 'foo',
+        user: 'tar',
+        accessTokenExpiresAt: atExpiresAt
+      };
+
+      try {
+        new TokenModel(data);
+        should.fail();
+      } catch (e) {
+        e.should.be.an.instanceOf(InvalidArgumentError);
+        e.message.should.equal('Missing parameter: `client`');
+      }
+    });
+    it('throws, if `user` is missing', function () {
+      const atExpiresAt = new Date();
+      atExpiresAt.setHours(new Date().getHours() + 1);
+
+      const data = {
+        accessToken: 'foo',
+        client: 'bar',
+        accessTokenExpiresAt: atExpiresAt
+      };
+
+      try {
+        new TokenModel(data);
+        should.fail();
+      } catch (e) {
+        e.should.be.an.instanceOf(InvalidArgumentError);
+        e.message.should.equal('Missing parameter: `user`');
+      }
+    });
+    it('throws, if `accessTokenExpiresAt` is not a Date', function () {
+      const data = {
+        accessToken: 'foo',
+        client: 'bar',
+        user: 'tar',
+        accessTokenExpiresAt: '11/10/2023'
+      };
+
+      try {
+        new TokenModel(data);
+        should.fail();
+      } catch (e) {
+        e.should.be.an.instanceOf(InvalidArgumentError);
+        e.message.should.equal('Invalid parameter: `accessTokenExpiresAt`');
+      }
+    });
+    it('throws, if `refreshTokenExpiresAt` is not a Date', function () {
+      const data = {
+        accessToken: 'foo',
+        client: 'bar',
+        user: 'tar',
+        refreshTokenExpiresAt: '11/10/2023'
+      };
+
+      try {
+        new TokenModel(data);
+        should.fail();
+      } catch (e) {
+        e.should.be.an.instanceOf(InvalidArgumentError);
+        e.message.should.equal('Invalid parameter: `refreshTokenExpiresAt`');
+      }
+    });
     it('should calculate `accessTokenLifetime` if `accessTokenExpiresAt` is set', function() {
       const atExpiresAt = new Date();
       atExpiresAt.setHours(new Date().getHours() + 1);
